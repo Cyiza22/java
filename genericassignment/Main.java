6badb4aef2a32583d8660d80cb3239ca65e3d1c8
Cyiza22 <h.cyiza@alustudent.com>
2025-04-28 11:02:31 +0200

    final changes

--- genericassignment/Main.java
+++ genericassignment/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         
         Pair<String, Integer> pair1 = new Pair<>("Age", 25);
-
         Pair<Integer, String> pair2 = new Pair<>(404, "Not Found");
+        Pair<String, Integer> pair8 = new Pair<>("Age", 25);
 
         System.out.println("Pair 1: " + pair1.toString());
         System.out.println("Pair 2: " + pair2.toString());
@@ -14,5 +14,10 @@ public class Main {
         System.out.println("Pair 5: " + Pair.create(1, 2).toString());
         System.out.println("Pair 6: " + Pair.create(1.5, 2.5).toString());
         System.out.println("Pair 7: " + Pair.create(1.5, "Hello").toString());
+        System.out.println("Is Pair 1 equal to Pair 2? " + pair1.equals(pair2));
+        System.out.println("Is Pair 1 equal to Pair 8? " + pair1.equals(pair8)); 
+        System.out.println("HashCode of Pair 1: " + pair1.hashCode());
+        System.out.println("HashCode of Pair 2: " + pair2.hashCode());
+        System.out.println("HashCode of Pair 3: " + pair8.hashCode());
     }
 }
